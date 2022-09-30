37c4c21fd0df3b1801a9810560ee30e07f146f7f
Diego Rodriguez <69642649+Diegoo3210@users.noreply.github.com>
2022-09-30 09:19:49 -0500

    Final

--- src/cafe/cafe.service.spec.ts
+++ src/cafe/cafe.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CafeService } from './cafe.service';
 import { faker } from '@faker-js/faker';
 import { CafeEntity } from './cafe.entity';
+import { TypeOrmTestingConfig } from 'src/shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
 
 describe('CafeService', () => {
   let service: CafeService;
@@ -9,9 +11,14 @@ describe('CafeService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CafeService],
+      imports: [...TypeOrmTestingConfig()],
+
+
     }).compile();
 
     service = module.get<CafeService>(CafeService);
+    repository = module.get<Repository<CafeEntity>>(getRepositoryToken(CafeEntity));
+
   });
 
   it('should be defined', () => {
